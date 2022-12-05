a50087d82628df0b9d59259d26619285f4c866e3
jmendo15 <72280816+jmendo15@users.noreply.github.com>
2022-12-05 11:35:22 -0800

    firebase deploy

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -1,11 +1,20 @@
-import { initializeApp } from "firebase/app"
-import { getAuth } from "firebase/auth"
-import { getFirestore } from "firebase/firestore"
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  // THE CONFIG FROM YOUR FIREBASE CONSOLE
+  apiKey: "AIzaSyCXuTrFDhQxBB9dfikejAqrLFvrMpmk9kE",
+  authDomain: "gamerz-af7ff.firebaseapp.com",
+  projectId: "gamerz-af7ff",
+  storageBucket: "gamerz-af7ff.appspot.com",
+  messagingSenderId: "739579287792",
+  appId: "1:739579287792:web:7cbb92c8ed741797a54a35",
+  measurementId: "G-H1587SXGY2"
 }
 
-export const app = initializeApp(firebaseConfig)
-export const auth = getAuth(app)
-export const db = getFirestore(app)
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
+
