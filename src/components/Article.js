338ff1db58789e2b9b21a561eac862ab48cb541e
jmendo15 <72280816+jmendo15@users.noreply.github.com>
2022-12-12 18:20:17 -0800

    Update Article.js

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,10 +1,47 @@
 import React from "react";
+import { SignIn, SignOut, useAuthentication } from "../services/authService";
+import { Link } from "react-router-dom";
+import { auth } from "./firebaseConfig";
+import wraith from "../images/wraith.png";
 
-const Article = ({ article }) => {
-  return (
+const LikeOnClick = () => {
+
+  const user = useAuthentication();
+  
+  !user ? (
+    <SignIn />
+  ) : (
     <div>
-      <h1>{article}</h1>
+      <Link to="/user" className="link">
+        {auth.currentUser.displayName}
+      </Link>
+      <SignOut />
     </div>
+  )
+};
+
+  return (
+    <>
+      <div>
+        <div className="content">
+            <h1 className="bold-title">
+              Insert Name of Game
+            </h1>
+            <button onClick={LikeOnClick} className="like-button">
+              {/* insert heart icon for like button*/}
+            </button>
+        </div>
+        <div className="article-image">
+          <img src={wraith} alt="" />
+        </div>
+      </div>
+      <div>
+        <p className="subtext">
+            Insert Description of game:
+            <br /> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </>
   );
 };
 
