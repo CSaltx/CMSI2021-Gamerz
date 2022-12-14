1463e02cd8bf5800e762f06a3a501af3d09721e9
CSaltx <cobytyler7@gmail.com>
2022-12-14 02:03:24 -0800

    added about page with responsiveness

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,9 @@ import React from "react";
 const About = () => {
   return (
     <div className="about-page">
-      <h4 className="bold red">YOUR NEW FAVORITE TEAM</h4>
+      <h4 className="bold red team">YOUR NEW FAVORITE TEAM</h4>
       <div id="content">
-        <h1 className="white bold team">
+        <h1 className="white bold">
           We are a team of <span className="red">GAMERZ</span> here for you
         </h1>
         <p className="white bold">
@@ -13,19 +13,36 @@ const About = () => {
           don't know how to quench!
         </p>
       </div>
-      <div className="about-page">
+      <div className="about-info">
         <div>
-          <i class="fa-solid fa-earth-americas white"></i>
+          <i class="fa-solid fa-earth-americas fa-6x white"></i>
           <p className="red bold">Background</p>
           <p className="white">
             We all come from a variety of backgrounds,
             <br /> here to support you
           </p>
         </div>
-        <div className="style">
-          <i class="fa-regular fa-face-smile-beam"></i>
+        <div>
+          <i class="fa-regular fa-6x fa-face-smile-beam white"></i>
+          <p className="red bold">Unique Tastes</p>
+          <p className="white">
+            We all grew up on different genres
+            <br /> so why shouldn't you
+          </p>
+        </div>
+        <div>
+          <i class="fa-regular fa-lightbulb fa-6x white"></i>
+          <p className="red bold">Creativity</p>
+          <p className="white">
+            As students, we thrive and imbue creativity
+            <br /> in every product
+          </p>
         </div>
       </div>
+      <h2 className="red bold">
+        As a gamer, we know the feeling <br /> so we{" "}
+        <span className="red">FIXED IT</span>
+      </h2>
     </div>
   );
 };
