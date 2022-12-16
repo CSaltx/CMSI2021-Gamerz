0e737a666744d19fd8adc1a2d6d2124e2dfb2a35
ThomasP677 <85772082+ThomasP677@users.noreply.github.com>
2022-12-15 22:52:24 -0800

    Update Genres.js

--- src/components/Genres.js
+++ src/components/Genres.js
@@ -1,23 +1,37 @@
-function Genres() {
-  return (
-    <>
-      <div className="genre-list">
-        <h1 className="red bold">Hello There</h1>
-        <div className="">
-          <img />
-        </div>
-        <div className="">
-          <img />
-        </div>
-        <div className="">
-          <img />
-        </div>
-        <div className="">
-          <img />
-        </div>
-      </div>
-    </>
-  );
+function Genres(){
+      return (
+        <>
+          <div className="id">
+            <Nav/>
+          </div>
+          <div className= "GenreList" div/>
+            <div className="RPG">
+              <Nav/>
+              <img id="https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.cloudflare.steamstatic.com%2Fsteam%2Fapps%2F489830%2Fheader.jpg&imgrefurl=https%3A%2F%2Fstore.steampowered.com%2Fapp%2F489830%2FThe_Elder_Scrolls_V_Skyrim_Special_Edition%2F&tbnid=O0Hb7UCvCharFM&vet=12ahUKEwjp0aqInej7AhVIk44IHes1DckQMygSegUIARCOAg..i&docid=xpzVuQ0ZvnihRM&w=460&h=215&q=skyrim&ved=2ahUKEwjp0aqInej7AhVIk44IHes1DckQMygSegUIARCOAg" alt="photo of Skyrim logo" img/>
+            </div>
+            <div className="OpenWorld">
+              <Nav/>
+              <img id="https://www.google.com/imgres?imgurl=https%3A%2F%2Fassets.nintendo.com%2Fimage%2Fupload%2Fc_fill%2Cw_1200%2Fq_auto%3Abest%2Ff_auto%2Fdpr_2.0%2Fncom%2Fen_US%2Fgames%2Fswitch%2Ft%2Fthe-legend-of-zelda-breath-of-the-wild-switch%2Fhero&imgrefurl=https%3A%2F%2Fwww.nintendo.com%2Fstore%2Fproducts%2Fthe-legend-of-zelda-breath-of-the-wild-switch%2F&tbnid=_97PzSeh9hXgpM&vet=12ahUKEwjc_sD-nej7AhXVl44IHQ7ND-8QMygAegUIARDvAQ..i&docid=PeFxXPHt7eRRtM&w=2400&h=1350&q=legend%20of%20zelda%20breath%20of%20the%20wild&ved=2ahUKEwjc_sD-nej7AhXVl44IHQ7ND-8QMygAegUIARDvAQ" alt="photo of Legend of Zelda" img/> 
+            </div>
+            <div className="FPS">
+              <Nav/>
+              <img id="https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.contentapi.ea.com%2Fcontent%2Fdam%2Fapex-legends%2Fimages%2F2019%2F01%2Fapex-featured-image-16x9.jpg.adapt.crop16x9.1023w.jpg&imgrefurl=https%3A%2F%2Fwww.ea.com%2Fgames%2Fapex-legends&tbnid=_rHqWuvhygo0AM&vet=12ahUKEwjx8cO2nOv7AhV4nI4IHewOBAIQMygAegUIARDEAQ..i&docid=Z7jSNarsGbgH2M&w=1023&h=575&itg=1&q=apex%20legends&ved=2ahUKEwjx8cO2nOv7AhV4nI4IHewOBAIQMygAegUIARDEAQ" alt="photo of Apex Legends" img/>
+            </div>
+            <div className="SandBox">
+              <Nav/>
+              <img id ="https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fen%2F5%2F51%2FMinecraft_cover.png&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FMinecraft&tbnid=SF1PfcY_FwHXHM&vet=12ahUKEwjxgO_knOv7AhWhoI4IHd-_ALYQMygAegUIARCjAg..i&docid=EX__o7CpQE8fOM&w=255&h=383&itg=1&q=minecraft&ved=2ahUKEwjxgO_knOv7AhWhoI4IHd-_ALYQMygAegUIARCjAg"  img/>
+            </div>
+            <div className="Puzzle">
+              <Nav/>
+              <img id="https://www.google.com/imgres?imgurl=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2Fe%2Fe0%2FSudoku_Puzzle_by_L2G-20050714_standardized_layout.svg%2F1200px-Sudoku_Puzzle_by_L2G-20050714_standardized_layout.svg.png&imgrefurl=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FSudoku&tbnid=0tueYiWjctsawM&vet=12ahUKEwjzsf-xnev7AhXFko4IHfIfCfsQMygAegUIARDoAQ..i&docid=2PH68oM51aR1nM&w=1200&h=1200&q=sudoku&ved=2ahUKEwjzsf-xnev7AhXFko4IHfIfCfsQMygAegUIARDoAQ" alt ="photo of sudoku"/>
+            </div>
+            <div className="Sports">
+              <Nav/>
+              <img id="https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn.akamai.steamstatic.com%2Fsteam%2Fapps%2F1919590%2Fheader.jpg%3Ft%3D1669997894&imgrefurl=https%3A%2F%2Fstore.steampowered.com%2Fapp%2F1919590%2FNBA_2K23%2F&tbnid=IyNx6C3mMbJPnM&vet=12ahUKEwjnzeDknev7AhXgho4IHc_tCYcQMygDegUIARCfAg..i&docid=3ja88K3XoJ1ujM&w=460&h=215&itg=1&q=nba%202k23&ved=2ahUKEwjnzeDknev7AhXgho4IHc_tCYcQMygDegUIARCfAg" alt ="photo of NBA2k23" img/>
+            </div>
+       </>
+      );
+      
 }
 
 export default Genres;
