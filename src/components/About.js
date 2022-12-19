d39e5c0a604f819eecd8ee11f53aeb080c8deb68
christineli1412 <christineli1412@gmail.com>
2022-12-19 02:37:26 -0800

    styled about page

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from "react";
 const About = () => {
   return (
     <div className="about-page">
-      <h4 className="bold red team">YOUR NEW FAVORITE TEAM</h4>
+      <h4 className="bold white team">YOUR NEW FAVORITE TEAM</h4>
       <div id="content">
         <h1 className="white bold">
           We are a team of <span className="red">GAMERZ</span> here for you
@@ -16,7 +16,7 @@ const About = () => {
       <div className="about-info">
         <div>
           <i className="fa-solid fa-earth-americas fa-6x white"></i>
-          <p className="red bold">Background</p>
+          <p className="white bold">Background</p>
           <p className="white">
             We all come from a variety of backgrounds,
             <br /> here to support you
@@ -24,7 +24,7 @@ const About = () => {
         </div>
         <div>
           <i className="fa-regular fa-6x fa-face-smile-beam white"></i>
-          <p className="red bold">Unique Tastes</p>
+          <p className="white bold">Unique Tastes</p>
           <p className="white">
             We all grew up on different genres
             <br /> so why shouldn't you
@@ -32,14 +32,14 @@ const About = () => {
         </div>
         <div>
           <i className="fa-regular fa-lightbulb fa-6x white"></i>
-          <p className="red bold">Creativity</p>
+          <p className="white bold">Creativity</p>
           <p className="white">
             As students, we thrive and imbue creativity
             <br /> in every product
           </p>
         </div>
       </div>
-      <h2 className="red bold">
+      <h2 className="white bold">
         As a gamer, we know the feeling <br /> so we{" "}
         <span className="red">FIXED IT</span>
       </h2>
